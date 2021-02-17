66590d6c8881aa244a1699d323371b9674458d75
poypolpak <76849134+poypolpak@users.noreply.github.com>
2021-02-17 18:27:55 +0700

    Update api_key.py

--- apipoi/api_key.py
+++ apipoi/api_key.py
@@ -1,11 +1,9 @@
 from binance.client import Client
 
 def test_net():
-    prototype1_key = '58Vy1luWGXy5lTf4oBDc3BRexG1Usmt1JS64NiYTR7nyaaIMUXAUvDcPsXCUzpY6'
-    prototype1_secret = 'cwZAGNOEtSNJLgr45TAqLXVOporuYQMyF3X5PRIqsoNHwfS8pwtSQJL9M1iTvbjd'
-    key = prototype1_key
-    secret = prototype1_secret
-    return key, secret
+    api_key = 'Input your API Key here'
+    api_secret = 'Input your API Secret here'
+    return api_key, api_secret
 
 def client_test_net():
     api_key, api_secret = test_net()
